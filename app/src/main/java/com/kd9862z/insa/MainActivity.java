b768bbdc1c0a6e75183cd1b62de2af5e92783118
kd9862z <kd9862z@gmail.com>
2019-10-25 21:28:15 +0900

    1.0

--- app/src/main/java/com/kd9862z/insa/MainActivity.java
+++ app/src/main/java/com/kd9862z/insa/MainActivity.java
@@ -2,15 +2,60 @@ package com.kd9862z.insa;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Dictionary> mArrayList;
+    private CustomAdapter mAdapter;
+    private int count = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.rv_list);
+        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+
+        mArrayList = new ArrayList<>();
+
+        mAdapter = new CustomAdapter( mArrayList);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
+                mLinearLayoutManager.getOrientation());
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+
+
+
+        Button buttonInsert = (Button)findViewById(R.id.button_main_insert);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                count++;
+
+                Dictionary data = new Dictionary(count+"","Apple" + count, "사과" + count);
+
+                //mArrayList.add(0, dict); //RecyclerView의 첫 줄에 삽입
+                mArrayList.add(data); // RecyclerView의 마지막 줄에 삽입
+
+                mAdapter.notifyDataSetChanged();             }
+        });
+
     }
 
 }
-
